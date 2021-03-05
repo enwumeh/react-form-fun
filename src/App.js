6482c95026ce0552933eb6757fb4c1e8ae98de92
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-03-04 21:16:16 -0600

    figuring out a better way to loop through and render previous state

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import "./index.css";
 // import axios from 'axios';
 
 function App() {
-  const [userData, setUserData] = useState({
+  const blankData = {
     name: " ",
     zodiac: " ",
     zodiacClicked: false,
     nameClicked: false,
-  });
+  }
+  const [userData, setUserData] = useState([{
+    ...blankData
+  }]);
 
   const updateInput = (e) => {
     e.preventDefault();
-    console.log("this is the event",e)
+    console.log("this is the event", e)
     // if (e.children === "name" && "zodiac") {
       if (e.target) {
-      setUserData({ name: e.target[0].value, zodiac: e.target[1].value, nameClicked: true, zodiacClicked: true });
+        setUserData([...userData, { name: e.target[0].value, zodiac: e.target[1].value, nameClicked: true, zodiacClicked: true }]);
+        console.log("userData is here ",userData)
     }
     // else if (e.className === "zodiac-data") {
     // setUserData({zodiac: e.target.value, zodiacClicked: true})
@@ -25,6 +29,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>This is the zodiac display thingy</h1>
       <form onSubmit={updateInput}>
         <label htmlFor="name">Name:</label>
         <input
@@ -47,20 +52,18 @@ function App() {
         <button>Submit</button>
       </form>
       <div>
-        {userData.nameClicked ? (
-          <p>Name:{userData.name}</p>
+        {userData[1].nameClicked ? (
+          
+          <p>Name:{userData[0].name}</p>
         ) : (
           <p> name not displaying</p>
           )}
-        {userData.zodiacClicked ? (
-          <p>Zodiac:{userData.zodiac}</p>
+        {userData[1].zodiacClicked ? (
+          <p>Zodiac:{userData[0].zodiac}</p>
         ) : (
           <p> zodiac not displaying</p>
         )}
       </div>
-      {/* {userData.zodiac}
-      {userData.nameClicked} */}
-
     </div>
   );
 }

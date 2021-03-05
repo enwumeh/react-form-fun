9224ab8e548c15c92377eb0b12584432564e51ce
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-03-05 01:44:17 -0600

    made it cute

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import "./index.css";
 function App() {
   const blankData = {
     name: " ",
-    zodiac: " ",
-    zodiacClicked: false,
+    message: " ",
+    messageClicked: false,
     nameClicked: false,
   }
   const [userData, setUserData] = useState([{
@@ -17,19 +17,20 @@ function App() {
   const updateInput = (e) => {
     e.preventDefault();
     console.log("this is the event", e)
-    // if (e.children === "name" && "zodiac") {
+    // if (e.children === "name" && "message") {
       if (e.target) {
-        setUserData([...userData, { name: e.target[0].value, zodiac: e.target[1].value, nameClicked: true, zodiacClicked: true }]);
+        setUserData([...userData, { name: e.target[0].value, message: e.target[1].value, nameClicked: true, messageClicked: true }]);
         console.log("userData is here ",userData)
     }
-    // else if (e.className === "zodiac-data") {
+    // else if (e.className === "message-data") {
     // setUserData({zodiac: e.target.value, zodiacClicked: true})
     // }
   };
 
   return (
     <div className="App">
-      <h1>This is the zodiac display thingy</h1>
+      <h1>This is a message display forum</h1>
+      <p>leave your name and a message. just for fun!</p>
       <form onSubmit={updateInput}>
         <label htmlFor="name">Name:</label>
         <input
@@ -40,9 +41,9 @@ function App() {
             console.log(e.target.value);
           }}
         />
-       <label htmlFor="name">Zodiac:</label>
+       <label htmlFor="name">Message:</label>
         <input
-          className="zodiac-data"
+          className="message-data"
           type="text"
           name="zodiac"
           onChange={(e) => {
@@ -52,17 +53,20 @@ function App() {
         <button>Submit</button>
       </form>
       <div>
-        {userData[1].nameClicked ? (
-          
-          <p>Name:{userData[0].name}</p>
-        ) : (
-          <p> name not displaying</p>
-          )}
-        {userData[1].zodiacClicked ? (
-          <p>Zodiac:{userData[0].zodiac}</p>
-        ) : (
-          <p> zodiac not displaying</p>
-        )}
+        {
+          !userData[0].nameClicked || userData[1].nameClicked && !userData[0].messageClicked || userData[1].messageClicked ? (
+            userData.map((userTxt,idx)=>
+              <div id={idx}>
+                <span className="labelText">name: </span><span className="userText">{userTxt.name}</span>
+                <br />
+               <span className="labelText">message: </span><span className="userText">{userTxt.message}</span>
+                <br></br>
+                <br></br>
+              </div>
+
+            )) : (
+              <p> name not displaying</p>
+            )}            
       </div>
     </div>
   );
